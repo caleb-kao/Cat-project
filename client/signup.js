172ccb50fe10f2de9e90542fa414adffda9e48b2
Caleb-Kao <kcaleb101@gmail.com>
2023-09-29 14:06:02 -0400

    2 steps forward 1 step back

---
+++ client/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx';
+
+// uncomment so that webpack can bundle styles
+// import styles from './scss/application.scss';
+
+// React.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('sign')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'client/index.js'),
+    // entry: {
+    //     main: path.resolve(__dirname, 'client/components/index.js')
+    //     // signup: path.resolve(__dirname, 'client/signup.js'),
+    // },
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        path: path.join(__dirname, "/dist"), // the bundle output path
+        filename: "bundle.js", // the name of the bundle
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html", // to import index.html file inside index.js
+        }),
+    ],
     mode: process.env.NODE_ENV,
     module: {
         rules: [
@@ -19,19 +27,30 @@ module.exports = {
                         presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
-            }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/, // styles files
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+                loader: "url-loader",
+                options: { limit: false },
+            },
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
-        })
-    ],
 
+    // plugins: [
+    //     new HtmlWebpackPlugin({ filename: 'index.html', template: path.resolve(__dirname, 'client/components/index.html'), chunks: ['index'], inject: 'body' })
+    //     // new HtmlWebpackPlugin({ filename: 'signup.html', template: path.resolve(__dirname, 'client/signup.html'), chunks: ['signup'] })
+    // ],
     devServer: {
-        static: {
-            directory: path.join(__dirname, './dist'),
-        },
+        port: 8080, // you can change the port
     },
+    // devServer: {
+    //     static: {
+    //         directory: path.join(__dirname, '/dist'),
+    //     },
+    // },
     devtool: 'eval-source-map',
 }
